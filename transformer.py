042e433460e75b26ae31ddd168585f40bab85647
HZEmpire <xhz1@connect.hku.hk>
2024-10-25 04:47:14 -0700

    Finalize encoder

--- transformer.py
+++ transformer.py
@@ -15,39 +15,46 @@ class Attention(nn.Module):
         self.head_num = head_num
         self.head_dim = embed_size // head_num
 
-        self.attention = nn.Linear(embed_size, embed_size, bias=False)
+        # Separate Q, K, V layers for each attention head
+        self.q = nn.Linear(embed_size, embed_size, bias=False)
+        self.k = nn.Linear(embed_size, embed_size, bias=False)
+        self.v = nn.Linear(embed_size, embed_size, bias=False)
         self.fc = nn.Linear(embed_size, embed_size)
 
     def forward(self, x, mask=None):
         N, seq_len, num_features = x.shape
-        q = self.attention(x).reshape(N, seq_len, self.head_num, self.head_dim).permute(0, 2, 1, 3)
-        k = self.attention(x).reshape(N, seq_len, self.head_num, self.head_dim).permute(0, 2, 1, 3)
-        v = self.attention(x).reshape(N, seq_len, self.head_num, self.head_dim).permute(0, 2, 1, 3)
+
+        # Split the embedding into heads
+        q = self.q(x).reshape(N, seq_len, self.head_num, self.head_dim).permute(0, 2, 1, 3)
+        k = self.k(x).reshape(N, seq_len, self.head_num, self.head_dim).permute(0, 2, 1, 3)
+        v = self.v(x).reshape(N, seq_len, self.head_num, self.head_dim).permute(0, 2, 1, 3)
 
         # Calculate
         score = torch.matmul(q, k.permute(0, 1, 3, 2)) / np.sqrt(self.head_dim)
         if mask is not None:
+            mask = mask.unsqueeze(1)  # (batch_size, 1, seq_len, seq_len)
             score = score.masked_fill(mask == 0, float('-inf'))
         weight = F.softmax(score, dim=-1)
         # Perhaps we can add dropout here
         # weight = self.dropout(weight)
-        out = torch.matmul(weight, v).permute(0, 2, 1, 3).reshape(N, seq_len, num_features)
+        out = torch.matmul(weight, v).permute(0, 2, 1, 3).reshape(N, seq_len, self.embed_size)
         out = self.fc(out)
+        return out
+
 
 class FNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, num_layers=1, dropout=0):
         super(FNN, self).__init__()
         assert num_layers >= 1
-        self.fc = nn.Linear(hidden_size, hidden_size)
         self.layers = nn.ModuleList(
             [nn.Sequential(
-                nn.ReLU(nn.Linear(hidden_size, hidden_size)),
+                nn.Linear(hidden_size, hidden_size),
+                nn.ReLU(),
                 nn.Dropout(dropout)
             ) for i in range(num_layers - 1)])
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, output_size)
         self.relu = nn.ReLU()
-        self.softmax = nn.Softmax()
 
     def forward(self, x):
         x = self.fc1(x)
@@ -55,8 +62,8 @@ class FNN(nn.Module):
         for layer in self.layers:
             x = layer(x)
         x = self.fc2(x)
-        x = self.softmax(x)
         return x
+
 
 class TransformerEncoder(nn.Module):
     def __init__(self, embed_size, head_num, hidden_size, num_layers=1, dropout=0):
@@ -71,8 +78,11 @@ class TransformerEncoder(nn.Module):
 
     def forward(self, x, mask=None):
         for layer in self.layers:
-            x = layer(x, mask)
+            x = layer[0](x, mask) + x
+            x = layer[1](x) + x
         return self.norm(x)
     
     def encode(self, x, mask=None):
-        return self.forward(x, mask)
+        x = self.forward(x, mask)
+        x = x.mean(dim=1)
+        return x
